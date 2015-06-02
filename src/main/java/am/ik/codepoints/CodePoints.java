e020ecd592f5874a445140ad78701d8e8d664cb6
Toshiaki Maki <makingx@gmail.com>
2015-06-03 02:00:56 +0900

    update

--- src/main/java/am/ik/codepoints/CodePoints.java
+++ src/main/java/am/ik/codepoints/CodePoints.java
@@ -1,5 +1,7 @@
 package am.ik.codepoints;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,14 +10,27 @@ public interface CodePoints {
 
     boolean containsAny(String s);
 
+    CodePoints union(CodePoints codePoints);
+
+    CodePoints subtract(CodePoints codePoints);
+
+    CodePoints intersect(CodePoints codePoints);
+
+    Set<Integer> getSet();
+
     public static class Wrapper implements CodePoints {
         private final Set<Integer> set;
 
-        protected Wrapper(Integer... codePoints) {
+        public Wrapper(Integer... codePoints) {
             this.set = new HashSet<>(codePoints.length);
             for (Integer codePoint : codePoints) {
                 set.add(codePoint);
             }
+        }
+
+        public Wrapper(Collection<Integer> codePoints) {
+            this.set = new HashSet<>();
+            this.set.addAll(codePoints);
         }
 
         @Override
@@ -48,6 +63,37 @@ public interface CodePoints {
                 }
             }
             return false;
+        }
+
+        @Override
+        public CodePoints union(CodePoints codePoints) {
+            Set<Integer> set = new HashSet<>(this.set);
+            set.addAll(codePoints.getSet());
+            return new Wrapper(set);
+        }
+
+        @Override
+        public CodePoints subtract(CodePoints codePoints) {
+            Set<Integer> set = new HashSet<>(this.set);
+            set.removeAll(codePoints.getSet());
+            return new Wrapper(set);
+        }
+
+        @Override
+        public CodePoints intersect(CodePoints codePoints) {
+            Set<Integer> set = new HashSet<>();
+            Set<Integer> target = codePoints.getSet();
+            for (Integer s : this.set) {
+                if (target.contains(s)) {
+                    set.add(s);
+                }
+            }
+            return new Wrapper(set);
+        }
+
+        @Override
+        public Set<Integer> getSet() {
+            return Collections.unmodifiableSet(this.set);
         }
     }
 }
